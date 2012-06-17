d0e95543e6d9f113f4e16df2390da11c07545430
slaw-ik <slaw-ik@yandex.ru>
2012-06-17 12:08:51 +0300

    zoom map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function popup(msg){
+function popup(msg) {
 //    jQuery('.description').show();
 
+}
+
+function my_function() {
+//    jQuery('.left_section').hide();
+//    jQuery('.map_section').css("margin-left","0");
 }
